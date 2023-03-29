612223f5a84f1d3df313cf037cd38b4e35a74fee
GENSSteven <roi_killer@hotmail.fr>
2023-03-29 08:21:20 +0200

    update btn CoDeco EN

---
+++ js/EN_BtnCoDeco.js
@@ -0,0 +1,38 @@
+const btnCo = document.getElementById('co');
+
+let changeCoDeco = localStorage.getItem("coDeco");
+
+let iconeProfil = document.getElementById('logoProfil');
+
+function connexion() {
+    localStorage.setItem("coDeco", "true");
+}
+
+function deconnexion() {
+    localStorage.setItem("coDeco", "false");
+}
+
+if (changeCoDeco == "true") {
+
+    btnCo.innerText = "Logout";
+    document.documentElement.style.setProperty('--ecritureBtnCo', '#1B1B1B');
+    document.documentElement.style.setProperty('--bgBtnCo', '#880000');
+    document.documentElement.style.setProperty('--borderBtnCo', '#000000');
+    document.documentElement.style.setProperty('--boxShadowBtnCo', '#000000');
+    document.documentElement.style.setProperty('--bgBtnCoHover', '#a50000');
+    document.documentElement.style.setProperty('--ecritureBtnCoHover', '#999999');
+    btnCo.setAttribute("onclick", "deconnexion()");
+    iconeProfil.style.display = "block";
+
+} else {
+
+    btnCo.innerText = "Login";
+    document.documentElement.style.setProperty('--ecritureBtnCo', '#999999');
+    document.documentElement.style.setProperty('--bgBtnCo', '#001ca8');
+    document.documentElement.style.setProperty('--borderBtnCo', '#000000');
+    document.documentElement.style.setProperty('--boxShadowBtnCo', '#000000');
+    document.documentElement.style.setProperty('--bgBtnCoHover', '#0023cf');
+    document.documentElement.style.setProperty('--ecritureBtnCoHover', '#e2e2e2');
+    btnCo.removeAttribute("onclick");
+    iconeProfil.style.display = "none";
+}
